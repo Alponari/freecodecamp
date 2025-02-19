12fdefbc7aaed17152504b81b1313cfca42ea6a2
Alponari <alponarijoseph@gmail.com>
2025-02-19 13:59:55 +0300

    introducing the madlibs app

---
+++ madlibs/player.py
@@ -0,0 +1,11 @@
+NAME = input("your nickname: ")
+adj = input("adjective: ")
+noun = input("what do you like? ")
+artist = input("favourite musician: ")
+vid = input("favorite youtuber: ")
+verb = input("hobby: ")
+
+
+madlib = "My name is {NAME} and i am so {adj}. He is the only one trying to get {noun}. He loves to listen to {artist} and watch {vid}. His favourite thing to do is {verb} and go to the gym. "
+
+print(madlib)
