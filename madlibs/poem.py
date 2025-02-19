872569025666f3e09f1e9c3dcfc199d605b5c6da
Alponari <alponarijoseph@gmail.com>
2025-02-19 19:18:42 +0300

    poem

---
+++ madlibs/poem.py
@@ -0,0 +1,11 @@
+import time
+
+color1 = input("Enter your favorite color: ")
+noun = input("Enter something that is blue in color: ")
+who = input("Celebrity crush: ")
+
+Poet = f"Roses are {color1}, the {noun} is blue and i love {who}!"
+
+print(Poet)
+
+time.sleep(10)
