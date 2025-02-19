f64cf4bb412c944c071ae697a0c479316727464d
Alponari <alponarijoseph@gmail.com>
2025-02-19 14:20:10 +0300

    player modification added formating to string

--- madlibs/player.py
+++ madlibs/player.py
@@ -6,6 +6,6 @@ vid = input("favorite youtuber: ")
 verb = input("hobby: ")
 
 
-madlib = "My name is {NAME} and i am so {adj}. He is the only one trying to get {noun}. He loves to listen to {artist} and watch {vid}. His favourite thing to do is {verb} and go to the gym. "
+madlib = f"My name is {NAME} and i am so {adj}. He is the only one trying to get {noun}. He loves to listen to {artist} and watch {vid}. His favourite thing to do is {verb} and go to the gym. "
 
 print(madlib)
